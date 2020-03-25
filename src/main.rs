0e9d2adbcc8b975fcb55267f0a21ec5fa242e1ca
HoodleWoodle <hoodlewoodle@web.de>
2020-03-25 01:56:34 +0100

    Initial commit #2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 use ggez::{graphics, Context, ContextBuilder, GameResult};
-use ggez::graphics::*;
+use ggez::graphics::{Rect, Mesh, DrawMode, DrawParam};
 use ggez::event::{self, EventHandler};
 
 fn main() {
-    // Make a Context.
     let (mut ctx, mut event_loop) = ContextBuilder::new("Game", "HoodleWoodle")
 		.build()
 		.expect("aieee, could not create ggez context!");
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
     let mut game = Game::new(&mut ctx);
 
-    // Run!
     match event::run(&mut ctx, &mut event_loop, &mut game) {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured: {}", e)
