d691f4bd429a21daa8d0cd156f0f52227b80e88d
HoodleWoodle <hoodlewoodle@web.de>
2020-03-26 21:34:50 +0100

    Its kind nice now

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,75 @@
 use ggez::{
-    Context, ContextBuilder, GameResult,
+    timer,
+    mint::Point2,
     conf::{WindowMode, WindowSetup},
     event::{self, EventHandler, KeyCode, KeyMods},
-    graphics::{self, Rect, Mesh, DrawMode, DrawParam, Color},
-    timer,
+    graphics::{self, Color, DrawMode, DrawParam, Mesh, Rect, Image},
+    audio::{Source, SoundSource},
+	Context, ContextBuilder, GameResult,
 };
 use rand::{self, Rng};
-use std::time::Duration;
+use std::{
+    process,
+    cmp::PartialEq,
+    time::Duration,
+    path::Path,
+    env,
+    path,
+};
 
 const FIELD_WIDTH: usize = 10;
-const FIELD_HEIGHT: usize = 16;
+const FIELD_HEIGHT: usize = 20;
 const FIELD_TILE_COUNT: usize = FIELD_WIDTH * FIELD_HEIGHT;
-
-const FIELD_OFFSET: usize = 1;
+const FIELD_OFFSET: usize = 2;
 
 const TILE_SIZE: usize = 32;
-const TILE_SPACING: usize = 1;
+const TILE_SPACING: usize = 0;
 
 const WINDOW_WIDTH: usize = TILE_SIZE * FIELD_WIDTH + TILE_SPACING * (FIELD_WIDTH - 1) + FIELD_OFFSET * 2;
 const WINDOW_HEIGHT: usize = TILE_SIZE * FIELD_HEIGHT + TILE_SPACING * (FIELD_HEIGHT - 1) + FIELD_OFFSET * 2;
 
-const COLOR_EMPTY: Color = graphics::WHITE;
-
 fn main() {
-    //TODO:
-    //- Sound
-    //- Icon
-    //- Score berechnen und anzeigen (siehe score())
-    //- Refactoring
-    //- Spielerlebnis: (z.B.: hard_drop und line removal schoener darstallen)
-    //- Game over + Retry Screen
-    //- Schoenere Tiles
+    // TODO:
+    // - Schoenere Tiles (image)
+    // - Score berechnen und anzeigen (siehe score())
+    // - Game over + Retry Screen
+    // - Refactoring
+        // - shadowed tetrimino wo tetrimino landen würde
+    // - line removal schoener darstellen
+    // - Hard drop (nicht sooo hard)
+    // - Soft drop
+
+    let mut ctx_builder = ContextBuilder::new("tetris", "");
+
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        ctx_builder = ctx_builder.add_resource_path(path);
+    }
 
     let window_mode = WindowMode::default()
         .dimensions(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
     let window_setup = WindowSetup::default()
-        .title("Tetris");
+        .title("Tetris")
+        .icon("/icon.png");
 
-    let (mut ctx, mut event_loop) = ContextBuilder::new("tetris", "author")
+    let (mut ctx, mut event_loop) = ctx_builder
         .window_mode(window_mode)
         .window_setup(window_setup)
         .build()
         .expect("Could not create ggez context!");
 
-    let mut state = GameState::new(&mut ctx);
+    let mut state = match GameState::new(&mut ctx) {
+        Ok(state) => state,
+        Err(e) => {
+            eprintln!("Could not create GameState: {}", e);
+            process::exit(1);
+        }
+    };
 
     match event::run(&mut ctx, &mut event_loop, &mut state) {
         Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e)
+        Err(e) => eprintln!("Error occured: {}", e),
     }
 }
 
@@ -59,23 +81,32 @@ struct Point {
 
 impl Point {
     fn new(x: usize, y: usize) -> Point {
-        Point {
-            x,
-            y,
-        }
+        Point { x, y }
     }
 }
 
 impl Default for Point {
     fn default() -> Point {
-        Point::new(0,0)
+        Point::new(0, 0)
     }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum TileType {
+    Cyan,
+    Yellow,
+    Purple,
+    Green,
+    Red,
+    Blue,
+    Orange,
+    Empty,
 }
 
 struct Tetrimino {
     tiles: [Point; 4],
     center: Option<usize>,
-    color: Color,
+    tile_type: TileType,
 }
 
 impl Tetrimino {
@@ -87,83 +118,132 @@ impl Tetrimino {
             3 => Tetrimino::new_s(),
             4 => Tetrimino::new_z(),
             5 => Tetrimino::new_j(),
-            6 => Tetrimino::new_l(),
-            _ => panic!("dead code"),
+            _ => Tetrimino::new_l(),
         }
     }
 
     fn new_i() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(3, 0), Point::new(4, 0), Point::new(5, 0), Point::new(6, 0)],
+            tiles: [
+                Point::new(3, 0),
+                Point::new(4, 0),
+                Point::new(5, 0),
+                Point::new(6, 0),
+            ],
             center: Some(1),
-            color: Color::new(0.0, 0.9375, 0.9375, 1.0),
+            tile_type: TileType::Cyan,
         }
     }
 
     fn new_o() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 0), Point::new(4, 1), Point::new(5, 0), Point::new(5, 1)],
+            tiles: [
+                Point::new(4, 0),
+                Point::new(4, 1),
+                Point::new(5, 0),
+                Point::new(5, 1),
+            ],
             center: None,
-            color: Color::new(0.9375, 0.9375, 0.0, 1.0),
+            tile_type: TileType::Yellow,
         }
     }
 
     fn new_t() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 0), Point::new(5, 0), Point::new(5, 1), Point::new(6, 0)],
+            tiles: [
+                Point::new(4, 0),
+                Point::new(5, 0),
+                Point::new(5, 1),
+                Point::new(6, 0),
+            ],
             center: Some(1),
-            color: Color::new(0.625, 0.0, 0.9375, 1.0),
+            tile_type: TileType::Purple,
         }
     }
 
     fn new_s() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 1), Point::new(5, 0), Point::new(5, 1), Point::new(6, 0)],
+            tiles: [
+                Point::new(4, 1),
+                Point::new(5, 0),
+                Point::new(5, 1),
+                Point::new(6, 0),
+            ],
             center: Some(2),
-            color: Color::new(0.0, 0.9375, 0.0, 1.0),
+            tile_type: TileType::Green,
         }
     }
 
     fn new_z() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 0), Point::new(5, 0), Point::new(5, 1), Point::new(6, 1)],
+            tiles: [
+                Point::new(4, 0),
+                Point::new(5, 0),
+                Point::new(5, 1),
+                Point::new(6, 1),
+            ],
             center: Some(2),
-            color: Color::new(0.9375, 0.0, 0.0, 1.0),
+            tile_type: TileType::Red,
         }
     }
 
     fn new_j() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 0), Point::new(5, 0), Point::new(6, 0), Point::new(6, 1)],
+            tiles: [
+                Point::new(4, 0),
+                Point::new(5, 0),
+                Point::new(6, 0),
+                Point::new(6, 1),
+            ],
             center: Some(1),
-            color: Color::new(0.0, 0.0, 0.9375, 1.0),
+            tile_type: TileType::Blue,
         }
     }
 
     fn new_l() -> Tetrimino {
         Tetrimino {
-            tiles: [ Point::new(4, 0), Point::new(4, 1), Point::new(5, 0), Point::new(6, 0)],
+            tiles: [
+                Point::new(4, 0),
+                Point::new(4, 1),
+                Point::new(5, 0),
+                Point::new(6, 0),
+            ],
             center: Some(2),
-            color: Color::new(0.9375, 0.625, 0.0, 1.0),
+            tile_type: TileType::Orange,
         }
     }
 }
 
 struct GameState {
-	current: Tetrimino,
-    map: [Color; FIELD_TILE_COUNT],
+    _sound: Source,
+    image: Image,
+
+    current: Tetrimino,
+    map: [TileType; FIELD_TILE_COUNT],
     timer: Duration,
     score: usize,
 }
 
 impl GameState {
-    pub fn new(_ctx: &mut Context) -> GameState {
-        GameState {
+    pub fn new(ctx: &mut Context) -> GameResult<GameState> {
+        let mut _sound = Source::new(ctx, Path::new("/sound.ogg"))?;
+        _sound.set_repeat(true);
+        _sound.set_volume(0.025);
+        _sound.play()?;
+
+        let image = Image::new(ctx, Path::new("/tiles.png"))?;
+
+        let state = GameState {
+            _sound,
+            image,
+
             current: Tetrimino::new_random(),
-            map: [COLOR_EMPTY; FIELD_TILE_COUNT],
+            map: [TileType::Empty; FIELD_TILE_COUNT],
             timer: Duration::default(),
             score: 0,
-        }
+        };
+
+        Ok(state)
     }
 
     fn rotate(&mut self) {
@@ -180,20 +260,20 @@ impl GameState {
         let mut new_tiles = [Point::default(); 4];
 
         for i in 0..4 {
-        	let x_old = self.current.tiles[i].x as isize;
-        	let y_old = self.current.tiles[i].y as isize;
+            let x_old = self.current.tiles[i].x as isize;
+            let y_old = self.current.tiles[i].y as isize;
 
-		let cx = center.x as isize;
-        	let cy = center.y as isize;
+            let cx = center.x as isize;
+            let cy = center.y as isize;
 
-		let x = cx - (y_old - cy);
-		let y = cy + (x_old - cx);
+            let x = cx - (y_old - cy);
+            let y = cy + (x_old - cx);
 
-		if self.collision(x, y) {
-    			return;
-		}
+            if self.collision(x, y) {
+                return;
+            }
 
-		new_tiles[i] = Point::new(x as usize, y as usize);
+            new_tiles[i] = Point::new(x as usize, y as usize);
         }
 
         self.current.tiles = new_tiles;
@@ -203,14 +283,14 @@ impl GameState {
         let mut new_tiles = [Point::default(); 4];
 
         for i in 0..4 {
-        	let x = self.current.tiles[i].x as isize + x_off;
-        	let y = self.current.tiles[i].y as isize + y_off;
+            let x = self.current.tiles[i].x as isize + x_off;
+            let y = self.current.tiles[i].y as isize + y_off;
 
-		if self.collision(x, y) {
-    			return true;
-		}
+            if self.collision(x, y) {
+                return true;
+            }
 
-		new_tiles[i] = Point::new(x as usize, y as usize);
+            new_tiles[i] = Point::new(x as usize, y as usize);
         }
 
         self.current.tiles = new_tiles;
@@ -218,11 +298,11 @@ impl GameState {
     }
 
     fn left(&mut self) {
-	self.mov(-1,0);
+        self.mov(-1, 0);
     }
 
     fn right(&mut self) {
-        self.mov(1,0);
+        self.mov(1, 0);
     }
 
     fn drop_hard(&mut self) {
@@ -230,25 +310,23 @@ impl GameState {
     }
 
     fn drop(&mut self) -> bool {
-        if !self.mov(0,1) {
+        if !self.mov(0, 1) {
             return true;
         }
 
-        // score - line removal
-        // level
         for &pos in self.current.tiles.iter() {
-            self.map[FIELD_WIDTH * pos.y + pos.x] = self.current.color;
+            self.map[FIELD_WIDTH * pos.y + pos.x] = self.current.tile_type;
         }
 
-	self.score();
+        self.score();
 
         let new_tet = Tetrimino::new_random();
 
-	for &pos in new_tet.tiles.iter() {
-    		if self.collision(pos.x as isize, pos.y as isize) {
-        		//TODO
-        		panic!("YOU FOOL!");
-    		}
+        for &pos in new_tet.tiles.iter() {
+            if self.collision(pos.x as isize, pos.y as isize) {
+                // TODO
+                panic!("YOU FOOL!");
+            }
         }
 
         self.current = new_tet;
@@ -261,8 +339,9 @@ impl GameState {
 
         for y in (0..FIELD_HEIGHT).rev() {
             let mut full = true;
+
             for x in 0..FIELD_WIDTH {
-                if self.map[FIELD_WIDTH * y + x] == COLOR_EMPTY {
+                if self.map[FIELD_WIDTH * y + x] == TileType::Empty {
                     full = false;
                     break;
                 }
@@ -275,7 +354,7 @@ impl GameState {
         }
 
         for i in 0..count {
-            for y in (lines[i+1]..lines[i]).rev() {
+            for y in (lines[i + 1]..lines[i]).rev() {
                 for x in 0..FIELD_WIDTH {
                     self.map[FIELD_WIDTH * (y + i + 1) + x] = self.map[FIELD_WIDTH * y + x];
                 }
@@ -284,39 +363,41 @@ impl GameState {
 
         for y in 0..count {
             for x in 0..FIELD_WIDTH {
-                self.map[FIELD_WIDTH * y + x] = COLOR_EMPTY;
+                self.map[FIELD_WIDTH * y + x] = TileType::Empty;
             }
         }
     }
 
     fn collision(&mut self, x: isize, y: isize) -> bool {
-		if x < 0 || y < 0 {
-    			return true;
-		}
+        if x < 0 || y < 0 {
+            return true;
+        }
 
-		let x = x as usize;
-		let y = y as usize;
+        let x = x as usize;
+        let y = y as usize;
 
-		if x >= FIELD_WIDTH || y >= FIELD_HEIGHT {
-    			return true;
-		}
+        if x >= FIELD_WIDTH || y >= FIELD_HEIGHT {
+            return true;
+        }
 
-		if self.map[y * FIELD_WIDTH + x] != COLOR_EMPTY {
-    			return true;
-		}
+        if self.map[y * FIELD_WIDTH + x] != TileType::Empty {
+            return true;
+        }
 
-		false
+        false
     }
-}
 
-fn draw_tile(ctx: &mut Context, pos: Point, col: Color) -> GameResult<()> {
-	let x = (FIELD_OFFSET + pos.x * (TILE_SIZE + TILE_SPACING)) as f32;
-	let y = (FIELD_OFFSET + pos.y * (TILE_SIZE + TILE_SPACING)) as f32;
+    fn draw_tile(&self, ctx: &mut Context, pos: Point, tile_type: TileType) -> GameResult<()> {
+        let x = (FIELD_OFFSET + pos.x * (TILE_SIZE + TILE_SPACING)) as f32;
+        let y = (FIELD_OFFSET + pos.y * (TILE_SIZE + TILE_SPACING)) as f32;
 
-	let rect = Rect::new(x, y, TILE_SIZE as f32, TILE_SIZE as f32);
-	let rect = Mesh::new_rectangle(ctx, DrawMode::fill(), rect, col)?;
-
-	graphics::draw(ctx, &rect, DrawParam::default())
+        let rect = Rect::new(((tile_type as i32) as f32) * 0.125, 0.0, 0.125, 1.0);
+        let draw_param = DrawParam::default()
+            .src(rect)
+            .dest(Point2 { x, y });
+        
+        graphics::draw(ctx, &self.image, draw_param)
+    }
 }
 
 impl EventHandler for GameState {
@@ -329,28 +410,27 @@ impl EventHandler for GameState {
         }
 
         timer::yield_now();
-       	Ok(())
+        Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, Color::new(0.65, 0.65, 0.65, 1.0));
+        graphics::clear(ctx, Color::new(0.75, 0.75, 0.75, 1.0));
 
         for y in 0..FIELD_HEIGHT {
-    		for x in 0..FIELD_WIDTH {
-        		let pos = Point { x, y };
-        		draw_tile(ctx, pos, self.map[y * FIELD_WIDTH + x])?;
-    		}
+            for x in 0..FIELD_WIDTH {
+                let pos = Point { x, y };
+                self.draw_tile(ctx, pos, self.map[y * FIELD_WIDTH + x])?;
+            }
         }
-        
+
         for &pos in self.current.tiles.iter() {
-            draw_tile(ctx, pos, self.current.color)?;
+            self.draw_tile(ctx, pos, self.current.tile_type)?;
         }
 
         graphics::present(ctx)
     }
 
-    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, repeat: bool)
-    {
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, repeat: bool, ) {
         if !repeat {
             match keycode {
                 KeyCode::Up => self.rotate(),
